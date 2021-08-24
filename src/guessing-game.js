2ed834d1a835ec8e5dbe67dff6a0318f3fb307aa
burik84 <avburik84@gmail.com>
2021-08-24 20:46:29 +0300

    feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNumber;
+        this.maxNumber;
+        this.middle;
+    }
 
     setRange(min, max) {
-
+        this.minNumber=min;
+        this.maxNumber=max;
     }
 
     guess() {
-
+        this.middle=Math.ceil((this.minNumber+this.maxNumber)/2)
+        return this.middle
     }
 
     lower() {
-
+        this.maxNumber=this.middle
     }
 
     greater() {
-
+        this.minNumber=this.middle
     }
 }
 
